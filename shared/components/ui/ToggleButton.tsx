0c87e2a37e368dd847552523797a20663bb522ce
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-06-25 07:56:33 +0200

    feat: add ToggleButton component

---
+++ shared/components/ui/ToggleButton.tsx
@@ -0,0 +1,22 @@
+import {cn} from "@/shared/utils/cn";
+import {
+  ToggleButton as AriaToggleButton,
+  ToggleButtonProps,
+} from "react-aria-components";
+
+export function ToggleButton({children, ...props}: ToggleButtonProps) {
+  return (
+    <AriaToggleButton
+      {...props}
+      className={cn(
+        "p-base rounded-base bg-control-background border-control-border flex cursor-pointer flex-col justify-center border transition-all",
+        "focus-visible:ring-control-border/50 outline-none focus-visible:ring-3",
+        "pressed:border-control-selected-border pressed:focus-visible:ring-control-selected-border/50",
+        "selected:bg-control-selected-background selected:border-control-selected-border selected:focus-visible:ring-control-selected-border/50",
+        "disabled:cursor-default disabled:opacity-50",
+        props.className,
+      )}>
+      {children}
+    </AriaToggleButton>
+  );
+}
