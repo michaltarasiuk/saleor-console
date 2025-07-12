6849cca71c13ee78c700f8a77834af7c24c5e50a
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-07-12 23:28:30 +0200

    fix: update layout of ContactInfo component to use grid for better responsiveness

--- app/(dashboard)/profile/_components/ContactInfo.tsx
+++ app/(dashboard)/profile/_components/ContactInfo.tsx
@@ -5,7 +5,7 @@ export function ContactInfo() {
   return (
     <dl
       className={cn(
-        "gap-x-large-400 gap-y-large-200 flex flex-col flex-wrap md:flex-row",
+        "gap-x-large-400 gap-y-large-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4",
       )}>
       <ContactField label="Email" value="a.stuart@leafygardens.com" />
       <ContactField label="Phone number" value="+1 416 123 4567" />
