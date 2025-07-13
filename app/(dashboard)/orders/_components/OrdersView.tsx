995b21ed43ca95cb089c1357e87720175f55739d
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-07-13 19:06:45 +0200

    fix: remove unnecessary wait in OrdersView component

--- app/(dashboard)/orders/_components/OrdersView.tsx
+++ app/(dashboard)/orders/_components/OrdersView.tsx
@@ -7,10 +7,7 @@ import {assertNever} from "@/shared/utils/assert-never";
 import {OrderViewContext} from "../_utils/order-view-context";
 import {OrdersGrid, OrdersGridSkeleton} from "./OrdersGrid";
 
-const wait = new Promise((resolve) => setTimeout(resolve, 1_000));
-
 export function OrdersView() {
-  use(wait);
   const {orderView} = use(OrderViewContext);
   switch (orderView) {
     case "grid":
