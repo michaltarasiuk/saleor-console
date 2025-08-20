db73d59eee66382354d5ea80cdf9c8d48c9a9bc9
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-08-20 10:00:15 +0200

    feat: add viewport configuration for responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import "@/styles/globals.css";
 
+import type {Viewport} from "next";
+
 import {ApolloProvider} from "@/graphql/ApolloProvider";
 
 import {Html} from "./_components/Html";
 import {RouterProvider} from "./_components/RouterProvider";
+
+export const viewport: Viewport = {
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
+};
 
 interface RootLayoutProps {
   children: React.ReactNode;
